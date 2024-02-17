4c9ab6858a15f6adb7368c2090aad7f3d9411249
zhangran653 <zhangran42@126.com>
2024-02-17 17:25:15 +0800

    regex scanner

--- regex/Scanner.py
+++ regex/Scanner.py
@@ -1,27 +1,45 @@
-from enum import Enum
+from enum import Enum, auto
 
 
 class TokenType(Enum):
     # single token
-    LEFT_PAREN = 0  # (
-    RIGHT_PAREN = 1  # )
-    LEFT_BRACE = 2  # {
-    RIGHT_BRACE = 3  # }
-    DOT = 4  # .
-    PLUS = 5  # +
-    LEFT_BRACKET = 6  # [
-    RIGHT_BRACKET = 7  # ]
-    MINUS = 8  # -
-    STAR = 9  # *
-    QUESTION = 10  # ?
-    # EQUAL = 11  # =
-    S_ANCHOR = 12  # ^
-    E_ANCHOR = 13  # $
-    ESCAPE = 14  # \
-    OR = 15  # |
-    CHAR = 16  # normal character
+    LEFT_PAREN = auto()  # (
+    RIGHT_PAREN = auto()  # )
+    LEFT_BRACE = auto()  # {
+    RIGHT_BRACE = auto()  # }
+    DOT = auto()  # .
+    PLUS = auto()  # +
+    LEFT_BRACKET = auto()  # [
+    RIGHT_BRACKET = auto()  # ]
+    MINUS = auto()  # -
+    STAR = auto()  # *
+    QUESTION = auto()  # ?
+    COMMA = auto()  # ,
+    S_ANCHOR = auto()  # ^
+    E_ANCHOR = auto()  # $
+    ESCAPE = auto()  # \
+    OR = auto()  # |
+    INT = auto()
+    LETTER = auto()
+    ASCII = auto()
+    CHAR = auto()  # normal character EXCEPT INT, LETTER ,ASCII
 
-    EOF = -1  # EOF
+    # ESCAPES
+    ANY_WORD = auto()  # \w
+    ANY_WORD_INVERTED = auto()  # \W
+    ANY_DIGIT = auto()  # \d
+    ANY_DIGIT_INVERTED = auto()  # \D
+    ANY_WHITE_SPACE = auto()  # \s
+    ANY_WHITE_SPACE_INVERTED = auto()  # \S
+
+    WORD_BOUND = auto()  # \b
+    NON_WORD_BOUND = auto()  # \B
+    START_OF_STRING_ONLY = auto()  # \A
+    END_OFF_STRING_ONLY_NOT_NEWLINE = auto()  # \z
+    END_OFF_STRING_ONLY = auto()  # \Z
+    PRE_MATCH_END = auto()  # \G
+
+    EOF = auto()  # EOF
 
 
 class Token:
@@ -55,36 +73,74 @@ class Scanner:
             '^': TokenType.S_ANCHOR,
             '$': TokenType.E_ANCHOR,
             '\\': TokenType.ESCAPE,
-            '|': TokenType.OR
+            '|': TokenType.OR,
+            ',': TokenType.COMMA
+        }
+        self.escape_map = {
+            'w': TokenType.ANY_WORD,
+            'W': TokenType.ANY_WORD_INVERTED,
+            'd': TokenType.ANY_DIGIT,
+            'D': TokenType.ANY_DIGIT_INVERTED,
+            's': TokenType.ANY_WHITE_SPACE,
+            'S': TokenType.ANY_WHITE_SPACE_INVERTED,
+            'b': TokenType.WORD_BOUND,
+            'B': TokenType.NON_WORD_BOUND,
+            'A': TokenType.START_OF_STRING_ONLY,
+            'z': TokenType.END_OFF_STRING_ONLY_NOT_NEWLINE,
+            'Z': TokenType.END_OFF_STRING_ONLY,
+            'G': TokenType.PRE_MATCH_END
         }
 
     def scan_tokens(self) -> list[Token]:
         while not self.is_end():
             self.start = self.current
-            self.scan_token()
+            self._scan_token()
         self.tokens.append(Token(TokenType.EOF))
         return self.tokens
 
     def is_end(self) -> bool:
         return self.current >= len(self.input)
 
-    def scan_token(self):
+    def _scan_token(self):
         char = self.advance()
         token_type = self.token_map.get(char, None)
+        # can be normal char or invalid char
         if not token_type:
             if self.is_valid_char(char):
-                self.add_token(TokenType.CHAR)
+                if char.isdigit():
+                    self.add_token(TokenType.INT, char)
+                elif (65 <= ord(char) <= 90) or (97 <= ord(char) <= 122):
+                    self.add_token(TokenType.LETTER, char)
+                elif char.isascii():
+                    self.add_token(TokenType.ASCII, char)
+                else:
+                    self.add_token(TokenType.CHAR, char)
             else:
-                raise ValueError(f"character {char} not supported")
+                raise ValueError(f"character {char} at {self.current} not supported")
         else:
-            pass
+            if token_type == TokenType.ESCAPE:
+                # escapes
+                next_char = self.peek()
+                if next_char == '\0':
+                    self.add_token(TokenType.ASCII, char)
+                else:
+                    if next_char in self.token_map:
+                        self.advance()
+                        self.add_token(TokenType.ASCII, next_char)
+                    elif next_char in self.escape_map:
+                        self.advance()
+                        self.add_token(self.escape_map[next_char], next_char)
+                    else:
+                        raise ValueError(f"character {char} at {self.current} can't be escaped")
+            else:
+                self.add_token(token_type, char)
 
     def advance(self) -> str:
         self.current += 1
         return self.input[self.current - 1]
 
-    def add_token(self, token_type: TokenType):
-        self.tokens.append(Token(token_type))
+    def add_token(self, token_type: TokenType, value: str = None):
+        self.tokens.append(Token(token_type, value))
 
     def match(self, expected: str) -> bool:
         if self.is_end():
---
+++ test/test_regex.py
@@ -0,0 +1,50 @@
+import unittest
+
+from regex.Scanner import Scanner
+
+
+class RegexTest(unittest.TestCase):
+    def test1(self):
+        scanner = Scanner('')
+        tokens = scanner.scan_tokens()
+        print(tokens)
+
+    def test2(self):
+        scanner = Scanner('a')
+        tokens = scanner.scan_tokens()
+        print(tokens)
+
+    def test3(self):
+        scanner = Scanner('ab+')
+        tokens = scanner.scan_tokens()
+        print(tokens)
+
+    def test4(self):
+        scanner = Scanner('[]')
+        tokens = scanner.scan_tokens()
+        print(tokens)
+
+    def test5(self):
+        scanner = Scanner('[a-zA-Z]')
+        tokens = scanner.scan_tokens()
+        print(tokens)
+
+    def test6(self):
+        scanner = Scanner('[a-zA-Z\[]')
+        tokens = scanner.scan_tokens()
+        print(tokens)
+
+    def test7(self):
+        scanner = Scanner('a{3,4}')
+        tokens = scanner.scan_tokens()
+        print(tokens)
+
+    def test8(self):
+        scanner = Scanner('ad阿松大')
+        tokens = scanner.scan_tokens()
+        print(tokens)
+
+    def test9(self):
+        scanner = Scanner('\s\\b\w\S\A\Z\G.|')
+        tokens = scanner.scan_tokens()
+        print(tokens)
